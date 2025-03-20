892030300dbe062b2b57eff6d92e765a47047958
adeliamj <adelia.miftakul.2205356@students.um.ac.id>
2025-03-20 11:34:06 +0700

    fix (Next.js config): build errors

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig = {
-  devIndicators: false,
-  eslint: {
-    ignoreDuringBuilds: true, 
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  devIndicators: false
 };
 
 module.exports = nextConfig;
